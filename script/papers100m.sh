db2e7d1dbf688e704d1be01bd24ea9cb7360280b
odeinjul <odeinjul@gmail.com>
2023-12-28 07:58:48 +0000

    upd

---
+++ script/papers100m.sh
@@ -0,0 +1,4 @@
+python3.10 ../multigpu.py --dataset_name "ogb-paper100M"  --mode mixed --gpu "0" --num_epochs 20 --hidden_dim 32
+python3.10 ../multigpu.py --dataset_name "ogb-paper100M"  --mode mixed --gpu "0,1" --num_epochs 20 --hidden_dim 32
+python3.10 ../multigpu.py --dataset_name "ogb-paper100M"  --mode mixed --gpu "0,1,2,3" --num_epochs 20 --hidden_dim 32
+python3.10 ../multigpu.py --dataset_name "ogb-paper100M"  --mode mixed --gpu "0,1,2,3,4,5,6,7" --num_epochs 20 --hidden_dim 32
