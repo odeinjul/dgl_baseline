83cb8a58652684c432807c51c63d63a97ae9b528
odeinjul <odeinjul@gmail.com>
2023-12-28 05:46:28 +0000

    update products.sh

--- script/products.sh
+++ script/products.sh
@@ -1,1 +1,4 @@
-python3.10 ../multigpu.py --dataset "ogb-product"  --mode mixed --gpu "0" --num_epochs 20 --hidden_dim 32
+python3.10 ../multigpu.py --dataset_name "ogb-products"  --mode mixed --gpu "0" --num_epochs 20 --hidden_dim 32
+python3.10 ../multigpu.py --dataset_name "ogb-products"  --mode mixed --gpu "0,1" --num_epochs 20 --hidden_dim 32
+python3.10 ../multigpu.py --dataset_name "ogb-products"  --mode mixed --gpu "0,1,2,3" --num_epochs 20 --hidden_dim 32
+python3.10 ../multigpu.py --dataset_name "ogb-products"  --mode mixed --gpu "0,1,2,3,4,5,6,7" --num_epochs 20 --hidden_dim 32
